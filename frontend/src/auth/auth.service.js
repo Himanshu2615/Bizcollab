736f04164b77aa67b0195a2257d8de91e084e1c8
Ananya <ananychaturvedi2003@gmail.com>
2026-03-19 20:20:00 +0530

    More changes to fix the revert

--- frontend/src/auth/auth.service.js
+++ frontend/src/auth/auth.service.js
@@ -7,8 +7,9 @@ import successHandler from '@/request/successHandler';
 export const login = async ({ loginData }) => {
   try {
     const response = await axios.post(
-      API_BASE_URL + `login?timestamp=${new Date().getTime()}`,
-      loginData
+      `login?timestamp=${new Date().getTime()}`,
+      loginData,
+      { baseURL: API_BASE_URL }
     );
 
     const { status, data } = response;
@@ -28,7 +29,7 @@ export const login = async ({ loginData }) => {
 
 export const register = async ({ registerData }) => {
   try {
-    const response = await axios.post(API_BASE_URL + `register`, registerData);
+    const response = await axios.post(`register`, registerData, { baseURL: API_BASE_URL });
 
     const { status, data } = response;
 
@@ -47,7 +48,7 @@ export const register = async ({ registerData }) => {
 
 export const verifyOTP = async ({ userId, otp }) => {
   try {
-    const response = await axios.post(API_BASE_URL + `verify-otp`, { userId, otp });
+    const response = await axios.post(`verify-otp`, { userId, otp }, { baseURL: API_BASE_URL });
 
     const { status, data } = response;
 
@@ -66,7 +67,7 @@ export const verifyOTP = async ({ userId, otp }) => {
 
 export const resendOTP = async ({ userId }) => {
   try {
-    const response = await axios.post(API_BASE_URL + `resend-otp`, { userId });
+    const response = await axios.post(`resend-otp`, { userId }, { baseURL: API_BASE_URL });
 
     const { status, data } = response;
 
@@ -85,7 +86,7 @@ export const resendOTP = async ({ userId }) => {
 
 export const verify = async ({ userId, emailToken }) => {
   try {
-    const response = await axios.get(API_BASE_URL + `verify/${userId}/${emailToken}`);
+    const response = await axios.get(`verify/${userId}/${emailToken}`, { baseURL: API_BASE_URL });
 
     const { status, data } = response;
 
@@ -104,7 +105,7 @@ export const verify = async ({ userId, emailToken }) => {
 
 export const resetPassword = async ({ resetPasswordData }) => {
   try {
-    const response = await axios.post(API_BASE_URL + `resetpassword`, resetPasswordData);
+    const response = await axios.post(`resetpassword`, resetPasswordData, { baseURL: API_BASE_URL });
 
     const { status, data } = response;
 
@@ -124,7 +125,7 @@ export const logout = async () => {
   axios.defaults.withCredentials = true;
   try {
     // window.localStorage.clear();
-    const response = await axios.post(API_BASE_URL + `logout?timestamp=${new Date().getTime()}`);
+    const response = await axios.post(`logout?timestamp=${new Date().getTime()}`, {}, { baseURL: API_BASE_URL });
     const { status, data } = response;
 
     successHandler(
@@ -142,7 +143,7 @@ export const logout = async () => {
 
 export const info = async () => {
   try {
-    const response = await axios.get(API_BASE_URL + `admin/read/current`);
+    const response = await axios.get(`admin/read/current`, { baseURL: API_BASE_URL });
 
     const { status, data } = response;
 
